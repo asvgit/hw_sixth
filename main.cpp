2e08a41daa2fa9dfc962e5cd975497b616ba5603
Арляпов Савелий <sava_arl@mail.ru>
2019-01-04 01:08:35 +0800

    make simple solution

--- main.cpp
+++ main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <cassert>
+#include <sstream>
 
-int main() {
+int main(int argc, char *argv[]) {
 	try {
-		std::cout << "hw_sixth" << std::endl;
+		assert(argc == 2);
+		const int bulk_size = [&]() {
+			std::stringstream ss(argv[1]);
+			int n;
+			ss >> n;
+			return n;
+		}();
+
+		std::string bulk;
+		std::string line;
+		int count = 0;
+		while (std::getline(std::cin, line)) {
+			if (bulk.empty())
+				bulk = line;
+			else
+				bulk += ", " + line;
+			if (++count == bulk_size) {
+				std::cout << "bulk: " << bulk << std::endl;
+				bulk.clear();
+				count = 0;
+			}
+		}
+
+		// std::cout << "hw_sixth" << std::endl;
 	} catch(const std::exception &e) {
 		std::cerr << e.what() << std::endl;
 	}
